46f6f3bb1cf606f0d4dbd26c40ab599f53ddc14d
Helge Holm <helge.holm@tv2.no>
2024-09-02 11:22:26 +0200

    Even less math

--- solution/reader.go
+++ solution/reader.go
@@ -4,13 +4,38 @@ import (
 	"iter"
 )
 
-var DIGITS = [58]int64{
+var DIGITS_1 = [58]int64{
 	0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 	0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+}
+
+var DIGITS_10 = [58]int64{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 10, 20, 30, 40, 50, 60, 70, 80, 90,
+}
+var DIGITS_100 = [58]int64{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 100, 200, 300, 400, 500, 600, 700, 800, 900,
+}
+var DIGITS_1000 = [58]int64{
+	0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+	0, 1000, 2000, 3000, 4000, 5000, 6000, 7000, 8000, 9000,
 }
 
 func Records(data []byte) iter.Seq[*Record] {
@@ -40,15 +65,15 @@ func consumeMeasurement2(data []byte, pos int, result *Decimal2) int {
 	}
 	switch fieldEnd - pos {
 	case 3: // x.x
-		*result = DIGITS[data[pos]]*100 + DIGITS[data[pos+2]]*10
+		*result = DIGITS_100[data[pos]] + DIGITS_10[data[pos+2]]
 	case 4: // x.xx or xx.x
 		if data[1] == '.' { // x.xx
-			*result = DIGITS[data[pos]]*100 + DIGITS[data[pos+2]]*100 + DIGITS[data[pos+3]]
+			*result = DIGITS_100[data[pos]] + DIGITS_100[data[pos+2]] + DIGITS_1[data[pos+3]]
 		} else { // xx.x
-			*result = DIGITS[data[pos]]*1000 + DIGITS[data[pos+1]]*100 + DIGITS[data[pos+3]]*10
+			*result = DIGITS_1000[data[pos]] + DIGITS_100[data[pos+1]] + DIGITS_10[data[pos+3]]
 		}
 	case 5: // xx.xx
-		*result = DIGITS[data[pos]]*1000 + DIGITS[data[pos+1]]*100 + DIGITS[data[pos+3]]*10 + DIGITS[data[pos+4]]
+		*result = DIGITS_1000[data[pos]] + DIGITS_100[data[pos+1]] + DIGITS_10[data[pos+3]] + DIGITS_1[data[pos+4]]
 	}
 	if negative {
 		*result = -*result
