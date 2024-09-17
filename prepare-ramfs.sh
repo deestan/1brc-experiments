08bc5be9bf157f9d648696721433cb9996c6e2b6
Helge Holm <helge.holm@tv2.no>
2024-09-17 09:15:29 +0200

    hugepages not needed for sequential read i thinko

--- prepare-ramfs.sh
+++ prepare-ramfs.sh
@@ -1,5 +1,5 @@
 # Put on ram drive! Run as sudo
 set -e
 mkdir -p ramfs
-mount -t tmpfs -omode=1777,huge=always,noswap tmpfs ./ramfs
+mount -t tmpfs -omode=1777,noswap tmpfs ./ramfs
 cp ./measurements.txt ./ramfs
